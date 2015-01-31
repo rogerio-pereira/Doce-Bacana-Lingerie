cf1328e7e58c98ebe0fa11705341b267bd923485
Rogério Pereira <rogerio@rogeriopereira.info>
2015-01-31 12:02:15 -0200

    continuação upload de arquivos

--- gestor/app.view/js/produto.js
+++ gestor/app.view/js/produto.js
@@ -255,6 +255,8 @@ function salvarProduto()
 	else
 		tam54 = 0;
 	
+	alert('aqui');
+	
 	$.ajax
 	({
 		type: "POST",
@@ -284,23 +286,6 @@ function salvarProduto()
 		{
 			alert(data);
 			//top.location='/produtos';
-		},
-		xhrFields: 
-		{
-			// add listener to XMLHTTPRequest object directly for progress (jquery doesn't have this yet)
-			onprogress: function (progress) 
-			{
-				// calculate upload progress
-				var percentage = Math.floor((progress.total / progress.totalSize) * 100);
-				// log upload progress to console
-				console.log('progress', percentage);
-				if (percentage === 100) 
-				{
-					console.log('DONE!');
-				}
-			}
-		},
-		processData: false,
-		contentType: multipart/form-data
+		}
 	});
 }
